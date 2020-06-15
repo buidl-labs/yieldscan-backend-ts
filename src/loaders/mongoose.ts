7854eee5e7ae41043ac22cabcd5a67b1f5d8b85b
Sahil <sahilnanda1995@gmail.com>
2020-06-15 12:14:52 +0530

    fix: 🐛 fixed DeprecationWarning
    current Server Discovery and Monitoring engine is deprecated, and will
    be removed in a future version. To use the new Server Discover and
    Monitoring engine, pass option { useUnifiedTopology: true } to the
    MongoClient constructor.

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,6 +3,6 @@ import { Db } from 'mongodb';
 import config from '../config';
 
 export default async (): Promise<Db> => {
-  const { connection } = await mongoose.connect(config.databaseURL, { useNewUrlParser: true, useCreateIndex: true });
+  const { connection } = await mongoose.connect(config.databaseURL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
   return connection.db;
 };
