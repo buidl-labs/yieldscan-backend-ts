a394afeffbf324f1f0e392807c64947d4f5dfd83
Sahil <sahilnanda1995@gmail.com>
2020-06-30 02:24:04 +0530

    feat: 🎸 added name parameter for session validators

--- src/api/middlewares/validatorsDashboard.ts
+++ src/api/middlewares/validatorsDashboard.ts
@@ -6,9 +6,22 @@ const validatorsDashboard = async (req, res, next) => {
   const Logger = Container.get('logger');
   try {
     const SessionValidators = Container.get('SessionValidators') as mongoose.Model<IStakingInfo & mongoose.Document>;
-    const sortedData = await SessionValidators.find({}).sort({
-      rewardsPer100KSM: -1,
-    });
+
+    const sortedData = await SessionValidators.aggregate([
+      {
+        $lookup: {
+          from: 'accountidentities',
+          localField: 'stashId',
+          foreignField: 'stashId',
+          as: 'info',
+        },
+      },
+      {
+        $sort: {
+          rewardsPer100KSM: -1,
+        },
+      },
+    ]);
     // console.log(sortedData);
     sortedData.map((x) => {
       x.commission = x.commission / Math.pow(10, 7);
@@ -17,6 +30,7 @@ const validatorsDashboard = async (req, res, next) => {
       x.othersStake = x.nominators.reduce((a, b) => a + b.stake, 0) / Math.pow(10, 12);
       x.numOfNominators = x.nominators.length;
       x.estimatedPoolReward = x.estimatedPoolReward / Math.pow(10, 12);
+      x.name = x.info[0].display;
     });
     // console.log('sortedData', sortedData);
     const result = sortedData.map(
@@ -29,6 +43,7 @@ const validatorsDashboard = async (req, res, next) => {
         numOfNominators,
         rewardsPer100KSM,
         riskScore,
+        name,
       }) => ({
         stashId,
         commission,
@@ -38,6 +53,7 @@ const validatorsDashboard = async (req, res, next) => {
         numOfNominators,
         rewardsPer100KSM,
         riskScore,
+        name,
       }),
     );
 
