a5c744fec7b3055d6a3de7f9daf52029f910d755
Sahil <sahilnanda1995@gmail.com>
2020-08-10 16:33:50 +0530

    fix: 🐛 added missing types

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import middlewares from '../middlewares';
 const route = Router();
 
-export default (app: Router) => {
+export default (app: Router): void => {
   // Register our endpoint for this route-apis
   app.use('/user', route);
 
