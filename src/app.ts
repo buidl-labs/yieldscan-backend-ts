d06ca0c0d25705e7b1d98da7f927385256310c7a
Sahil <sahilnanda1995@gmail.com>
2020-09-17 13:53:25 +0530

    refactor: 💡 app listening on port 3000

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata'; // We need this in order to use @Decorators
 
-// import express from 'express';
+import express from 'express';
 
-// import config from './config';
+import config from './config';
 
 import Logger from './loaders/logger';
 
@@ -23,23 +23,20 @@ startCrawlers().catch((error) => {
   process.exit(1);
 });
 
-// async function startServer() {
-//   const app = express();
+async function startServer() {
+  const app = express();
 
-//   await loaders({ expressApp: app });
+  app.listen(config.port, (err) => {
+    if (err) {
+      Logger.error(err);
+      process.exit(1);
+    }
+    Logger.info(`
+      ################################################
+      🛡️  Server listening on port: ${config.port} 🛡️
+      ################################################
+    `);
+  });
+}
 
-//   app.listen(config.port, (err) => {
-//     if (err) {
-//       Logger.error(err);
-//       process.exit(1);
-//       return;
-//     }
-//     Logger.info(`
-//       ################################################
-//       🛡️  Server listening on port: ${config.port} 🛡️
-//       ################################################
-//     `);
-//   });
-// }
-
-// startServer();
+startServer();
