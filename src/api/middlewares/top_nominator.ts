faeda1e602b89bd6ea5b8e2230f7acab19ba1f9c
Sahil <sahilnanda1995@gmail.com>
2020-09-18 21:50:14 +0530

    feat: 🎸 added twitter endpoints

---
+++ src/api/middlewares/top_nominator.ts
@@ -0,0 +1,56 @@
+import { Container } from 'typedi';
+import mongoose from 'mongoose';
+import { ITotalRewardHistory } from '../../interfaces/ITotalRewardHistory';
+import { INominatorHistory } from '../../interfaces/INominatorHistory';
+
+const top_nominator = async (req, res, next) => {
+  const Logger = Container.get('logger');
+  try {
+    const TotalRewardHistory = Container.get('TotalRewardHistory') as mongoose.Model<
+      ITotalRewardHistory & mongoose.Document
+    >;
+    const lastIndexDB = await TotalRewardHistory.find({}).sort({ eraIndex: -1 }).limit(1);
+
+    const eraIndex = lastIndexDB[0].eraIndex;
+    const eraTotalReward = lastIndexDB[0].eraTotalReward;
+
+    const NominatorHistory = Container.get('NominatorHistory') as mongoose.Model<INominatorHistory & mongoose.Document>;
+    const sortedData = await NominatorHistory.aggregate([
+      {
+        $match: {
+          eraIndex: eraIndex,
+        },
+      },
+    ]);
+
+    sortedData.map((x) => {
+      x.nomEraReward = x.validatorsInfo.reduce((a, b) => {
+        const commission = b.commission / Math.pow(10, 9);
+        const totalStake = b.totalStake / Math.pow(10, 12);
+        const nomStake = b.nomStake / Math.pow(10, 12);
+        const poolReward = ((eraTotalReward / Math.pow(10, 12)) * b.eraPoints) / b.totalEraPoints;
+        const reward = (poolReward - commission * poolReward) * (nomStake / totalStake);
+        return a + reward;
+      }, 0);
+      x.nomTotalStake = x.validatorsInfo.reduce((a, b) => {
+        const nomStake = b.nomStake / Math.pow(10, 12);
+        return a + nomStake;
+      }, 0);
+      x.nominations = x.validatorsInfo.length;
+    });
+
+    const max = sortedData.reduce(
+      (prev, current) => {
+        return prev.nomEraReward > current.nomEraReward ? prev : current;
+      },
+      { nomEraReward: 0 },
+    );
+
+    return res.status(200).json(max);
+  } catch (e) {
+    Logger.error('🔥 Error fetching max-set: %o', e);
+    return next(e);
+  }
+};
+
+export default top_nominator;
---
+++ src/api/routes/twitter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import middlewares from '../middlewares';
+const route = Router();
+
+export default (app: Router): void => {
+  // Register our endpoint for this route-apis
+  app.use('/twitter', route);
+
+  route.get('/top-validator', middlewares.top_validator);
+  route.get('/top-nominator', middlewares.top_nominator);
+};
